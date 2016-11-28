00206521119b3b8c1efa021df5313a0951aa3253
Manan V. Panchal <mananvpanchal@gmail.com>
2016-11-28 13:41:10 +0530

    routing with lazy loading module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,17 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from "@angular/router";
 
-import { HomeModule } from './home/home.module';
-
 import { AppComponent }  from './app.component';
-import { HomeComponent } from './home/home.component';
 
 const routerConfig = [
     {
       path: "home",
-      component: HomeComponent
+      loadChildren: './home/home.module'
     }
 ];
 
 @NgModule({
-  imports:      [ BrowserModule, HomeModule, RouterModule, RouterModule.forRoot(routerConfig) ],
+  imports:      [ BrowserModule, RouterModule, RouterModule.forRoot(routerConfig) ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { SampleModule } from '../sample/sample.module';
 import { HomeComponent } from './home.component';
 
+const routerConfig = [
+  {
+    path: "",
+    component: HomeComponent
+  }
+];
+
 @NgModule({
-  imports: [SampleModule],
+  imports: [SampleModule, RouterModule.forChild(routerConfig)],
   declarations: [HomeComponent],
   exports: [HomeComponent]
 })
-
-export class HomeModule {
+export default class HomeModule {
 
 }
